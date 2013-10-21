36884a0aec000d4230a385774edc3847b115a07f
Geoffrey Yoccoz <geoffrey.yoccoz@gmail.com>
2013-10-21 11:20:22 +0200

    Ajout script compilation

---
+++ Tex/compiler.bat
@@ -0,0 +1,9 @@
+del etat_de_l_art.aux
+del etat_de_l_art.toc
+del etat_de_l_art.log
+del etat_de_l_art.pdf
+pdflatex etat_de_l_art.tex
+echo off
+cls
+echo Compilation terminee && pause>nul
+etat_de_l_art.pdf
