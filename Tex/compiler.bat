5227927b9d391e86c7f49e70e481f9f2dbcfba23
charlyBerthet <charlyberthet@orange.fr>
2013-10-21 13:44:39 +0200

    MAJ compiler.bar
    Compile 3x pour table des matières

--- Tex/compiler.bat
+++ Tex/compiler.bat
@@ -3,6 +3,8 @@ del etat_de_l_art.toc
 del etat_de_l_art.log
 del etat_de_l_art.pdf
 pdflatex etat_de_l_art.tex
+pdflatex etat_de_l_art.tex
+pdflatex etat_de_l_art.tex
 echo off
 cls
 echo Compilation terminee && pause>nul
