7a2d27a0caf7a67f68cc28df4c5a095e8b07e329
Geoffrey Yoccoz <geoffrey.yoccoz@gmail.com>
2013-11-15 13:39:05 +0100

    MAJ script compiler.bat

--- Tex/compiler.bat
+++ Tex/compiler.bat
@@ -1,11 +1,11 @@
+cls
+@echo off
 del etat_de_l_art.aux
 del etat_de_l_art.toc
-del etat_de_l_art.log
 del etat_de_l_art.pdf
-pdflatex etat_de_l_art.tex
-pdflatex etat_de_l_art.tex
-pdflatex etat_de_l_art.tex
-echo off
-cls
-echo Compilation terminee by Emiliooooo Berthet && echo Appuyez sur une touche pour ouvrir le PDF && pause>nul
-etat_de_l_art.pdf
+echo 1ere compilation ...
+pdflatex etat_de_l_art.tex>nul
+echo 2eme compilation ...
+pdflatex etat_de_l_art.tex>nul
+pdflatex etat_de_l_art.tex>nul
+start etat_de_l_art.pdf
